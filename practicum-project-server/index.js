a65fb5812ae6585f01f9ba54c3fbc9f6eaf68478
MMamunurRashid <mdmamun.iubat.m@gmail.com>
2023-10-11 00:34:23 +0600

    initial server setup

--- practicum-project-server/index.js
+++ practicum-project-server/index.js
@@ -1,1 +1,33 @@
-console.log("Hello World")
+const express = require("express");
+const cors = require("cors");
+
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+async function run() {
+    try {  
+
+      
+          });
+    
+    } finally {
+    }
+  }
+  run().catch((err) => console.error(err));
+
+app.get("/", async (req, res) => {
+    res.send("First Office app!!! running");
+  });
+  app.listen(port, () => {
+    console.log(`First Office app!! running on port: ${port}`);
+  });
