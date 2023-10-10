d537d9df172f0262cb814b988341d62babd60d98
MMamunurRashid <mdmamun.iubat.m@gmail.com>
2023-10-11 00:28:20 +0600

    Initial Setup

---
+++ prcticum-project-client/src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="">
+     <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ prcticum-project-client/src/Pages/LoginAndRegister/Register/Register.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            Hello Register
+        </div>
+    );
+};
+
+export default Register;
